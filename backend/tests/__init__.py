31991383a37a68f1841c4690462c2ad1f6bab026
matthupy <matthupy@gmail.com>
2022-01-31 19:03:45 -0600

    Renamed Test Classes to use Camel Case

--- backend/tests/__init__.py
+++ backend/tests/__init__.py
@@ -1,6 +1,6 @@
 # Models
-from .models.test_agreement import *
-from .models.test_agreement_type import *
-from .models.test_rights import *
-from .models.test_subject_type import *
-from .models.test_well import *
+from .models.AgreementTests import *
+from .models.AgreementTypeTests import *
+from .models.RightsTests import *
+from .models.SubjectTypeTests import *
+from .models.WellTests import *
